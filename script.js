9e287e889a8cdcf09709dfa5f11c82cbacb59851
ElK <62822011+evkonradi@users.noreply.github.com>
2020-06-21 13:17:37 -0500

    changing background depending on the time of the day

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-var setDateHeader = function(){
-    $("#currentDay").text(moment().format("dddd, MMMM Mo"));
-     
+var changeCalendarBG = function(){
+    for (i = 9; i < 18; i++){
+        var el = $("#" + i);
+        el.removeClass("past present future");
+        currentHour = parseInt(moment().format("H"));
+        if (i < currentHour)
+            el.addClass("past");
+        else if (i == currentHour)
+            el.addClass("present");
+        else
+            el.addClass("future");
+    }
 }
 
-setDateHeader();
+var initialLoad = function(){
+    $("#currentDay").text(moment().format("dddd, MMMM Mo"));
+
+    changeCalendarBG();
+    setInterval(changeCalendarBG, 1000 * 60 ); //every minute
+}
+
+initialLoad();

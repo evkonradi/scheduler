f90199225c6c04fed012caa699cb399a96691048
ElK <62822011+evkonradi@users.noreply.github.com>
2020-06-21 21:07:42 -0500

    edit and save functionality

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler 
+
+This is the Work Day Scheduler that allows to enter and save to the Local Storage events for each working hour of the day (9am - 5pm).
+Events with the past time show in gray, current hour is in red, and future events are in green. 
+Events currently edited are outlined by blue border. To save an event you need to click the "Save" button near the event.
+
+It is built by using HTML, CSS, JavaScript, Bootstrap and jQuery.
+
+# Deployed web site
+
+The link to deployed site is https://evkonradi.github.io/scheduler/.
+
+# GitHub repository
+
+You can find the repository here: https://github.com/evkonradi/scheduler.
+
+# Page preview
+
+This is the preview of the initial page of the quiz:
+<img src="PagePreview.JPG">
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+var dayCalendar = {};
+
 var changeCalendarBG = function(){
     for (i = 9; i < 18; i++){
-        var el = $("#" + i);
+        var el = $("#" + i + "-description");
         el.removeClass("past present future");
         currentHour = parseInt(moment().format("H"));
         if (i < currentHour)
@@ -12,11 +14,64 @@ var changeCalendarBG = function(){
     }
 }
 
+var loadData = function(){
+    for (i = 9; i < 18; i++){
+        var el = $("#" + i + "-description");
+        el.find("span").text(dayCalendar[i-9]);
+    }
+}
+
 var initialLoad = function(){
     $("#currentDay").text(moment().format("dddd, MMMM Mo"));
 
     changeCalendarBG();
     setInterval(changeCalendarBG, 1000 * 60 ); //every minute
+
+    dayCalendar = JSON.parse(localStorage.getItem("dayCalendar"));
+
+    if (!dayCalendar){
+        dayCalendar = ['', '', '', '', '', '', '', '', '']; 
+    }
+    else 
+        loadData();
+
 }
+
+$(".container .row").on("click", ".col-10", function(){
+    $(this).addClass("border-blue");
+    var spanEl = $(this).find("span");
+  
+    var textAreaEl = $("<textarea>").val(spanEl.text().trim());
+    textAreaEl.addClass("outline-none");
+    $(this).find("span").replaceWith(textAreaEl);
+    textAreaEl.trigger("focus");
+});
+
+
+$(".container .row").on("click", ".saveBtn", function(){
+
+    var descEl = $(this).closest(".row").find(".col-10");
+    if (!descEl.hasClass("border-blue"))
+        return;
+    
+    descEl.removeClass("border-blue");
+    var textAreaEl = descEl.find("textarea");
+
+    var spanEl = $("<span>").text(textAreaEl.val());
+    textAreaEl.replaceWith(spanEl);
+
+    var index = parseInt(spanEl.closest("div").attr("id").replace("-description",""));
+    dayCalendar[index-9]=spanEl.text();
+
+    localStorage.setItem("dayCalendar", JSON.stringify(dayCalendar));
+});
+
+
+/*$(".container").on("focusout",".row", function(){
+    var textAreaEl = $(this).find(".col-10").find("textarea");
+    textAreaEl.replaceWith(spanEl);
+    spanEl = null;
+});*/
+  
 
 initialLoad();
